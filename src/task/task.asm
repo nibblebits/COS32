ac5b3e52b3fb443b40cb7b6db52e9f9f273f15df
Daniel McCarthy/AKA NibbleBits <dansblackberry2@hotmail.co.uk>
2020-07-23 21:39:26 +0100

    Fixed a bug where I overwrote EBX too soon!

--- src/task/task.asm
+++ src/task/task.asm
@@ -26,28 +26,50 @@ user_mode_enter_old:
 
 
 
-; uint32_t edi;
-  ;  uint32_t esi;
-  ;  uint32_t ebp;
-  ;  uint32_t ebx;
-  ;  uint32_t edx;
-  ;;  uint32_t ecx;
-  ;  uint32_t eax;
+; uint32_t edi; 0
+  ;  uint32_t esi; 4
+  ;  uint32_t ebp; 8
+  ;  uint32_t ebx; 12
+  ;  uint32_t edx; 16
+  ;;  uint32_t ecx; 20
+ ;  uint32_t eax; 24
 
-   ; uint32_t ip;
-  ;  uint32_t cs;
+   ; uint32_t ip; 28
+  ;  uint32_t cs; 32 
   ;  uint32_t flags;
    ; uint32_t esp;
    ; uint32_t ss;
 
 
+
+
 user_mode_enter:
-    push esp
     mov ebp, esp
-    push dword [ebp+8] ; The pointer to the registers
+
+    ; NOTE WE NEED TO RESTORE FLAGS AT SOME POINT ALSO!!!! FOR NOW WE WILL PUSH WHAT WE HAVE! NEEDS IMPROVEMENT!!!!
+    ; HERE WE SET THE INTERRUPT FLAG AS WE CAN'T DO THIS IN USER LAND
+    pushf
+    pop eax
+    or eax, 0x200
+    push eax
+
+    ; Let's access the structure passed to us
+    mov ebx, [ebp+4]
+    ; Now comes the code segment. 
+    push dword [ebx+32]
+    
+    ; Now comes the IP
+    push dword [ebx+28]
+
+    ; Restore the general purpose registers
+    push dword [ebp+4] ; The pointer to the registers
     call restore_general_purpose_registers
-    pop esp
-    ret
+    ; Let's discard the last item
+    add esp, 4
+
+    ; Consider using popa in future to avoid these complications
+
+    iretd
 
 ; void restore_general_purpose_registers(struct registers* registers)
 restore_general_purpose_registers:
@@ -57,10 +79,10 @@ restore_general_purpose_registers:
     mov edi, [ebx]
     mov esi, [ebx+4]
     mov ebp, [ebx+8]
-    mov ebx, [ebx+12]
     mov edx, [ebx+16]
     mov ecx, [ebx+20]
     mov eax, [ebx+24]
+    mov ebx, [ebx+12]
     pop esp
     ret
 
