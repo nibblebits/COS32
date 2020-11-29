c5855ed52a545743fbe53c6a88e9c7836e6f8bf8
Daniel McCarthy/AKA NibbleBits <dansblackberry2@hotmail.co.uk>
2020-11-29 12:31:44 +0000

    Fixed the COS32.asm user implementation, it now adheres to the C binary compatability standard!

--- src/programs/stdlib/src/cos32.asm
+++ src/programs/stdlib/src/cos32.asm
@@ -55,8 +55,7 @@ kernel_information:
     push ebp
     mov ebp, esp
     mov eax, 3 ; Command 3 = Kernel information
-    mov ebx, [ebp+8] ; The "struct kern_info" structure
-    push dword ebx
+    push dword [ebp+8]
     int 0x80
     add esp, 4
     pop ebp
@@ -74,8 +73,7 @@ cos32_malloc:
     push ebp
     mov ebp, esp
     mov eax, 5 ; Command 5 = malloc for entire process
-    mov ebx, [ebp+8] ; Size in bytes
-    push dword ebx
+    push dword [ebp+8] ; Size in bytes
     int 0x80
     add esp, 4
     pop ebp
@@ -85,8 +83,7 @@ cos32_invoke_command:
     push ebp
     mov ebp, esp
     mov eax, 6 ; Command 6 = Invoke command. SYSTEM command essentially
-    mov ebx, [ebp+8] ; The pointer to the "command_argument" structure
-    push dword ebx
+    push dword [ebp+8] ; The pointer to the "command_argument" structure
     int 0x80
     add esp, 4
     pop ebp
@@ -96,8 +93,7 @@ cos32_sleep:
     push ebp
     mov ebp, esp
     mov eax, 7 ; Command 7 = Task sleep. The task is put to sleep
-    mov ebx, [ebp+8] ; Total milli seconds to sleep for
-    push dword ebx
+    push dword [ebp+8] ; Total milli seconds to sleep for
     int 0x80
     add esp, 4
     pop ebp
@@ -107,14 +103,10 @@ cos32_video_rectangle_new:
     push ebp
     mov ebp, esp
     mov eax, 8 ; Comamnd 8 = Video rectangle new. Creates a new video rectangle
-    mov ebx, [ebp+8] ; X
-    push dword ebx
-    mov ebx, [ebp+12] ; Y
-    push dword ebx
-    mov ebx, [ebp+16] ; Width
-    push dword ebx
-    mov ebx, [ebp+20] ; Height
-    push dword ebx
+    push dword [ebp+8] ; X
+    push dword [ebp+12] ; Y
+    push dword [ebp+16] ; Width
+    push dword [ebp+20] ; Height
     int 0x80
     add esp, 16
     pop ebp
@@ -124,14 +116,10 @@ cos32_video_rectangle_set_pixel:
     push ebp
     mov ebp, esp
     mov eax, 9 ; Command 9 = video set pixel. Sets a pixel in this rectangle
-    mov ebx, [ebp+8] ; The video rectangle
-    push ebx
-    mov ebx, [ebp+12] ; X coordinate
-    push ebx
-    mov ebx, [ebp+16] ; Y coordinate
-    push ebx
-    mov ebx, [ebp+20] ; The colour of the pixel
-    push ebx
+    push dword [ebp+8] ; The video rectangle
+    push dword [ebp+12] ; X coordinate
+    push dword [ebp+16] ; Y Coordinate
+    push dword [ebp+20] ; The colour of the pixel
     int 0x80
     add esp, 16
     pop ebp
@@ -140,11 +128,9 @@ cos32_video_rectangle_set_pixel:
 cos32_video_rectangle_fill:
     push ebp
     mov ebp, esp
-    mov eax, 10 ; COmmand 10 = video fill. Fills the rectangle with a given colour
-    mov ebx, [ebp+8] ; The video rectangle
-    push ebx
-    mov ebx, [ebp+12] ; The colour
-    push ebx
+    mov eax, 10 ; Command 10 = video fill. Fills the rectangle with a given colour
+    push dword [ebp+8] ; The video rectangle
+    push dword [ebp+12] ; The colour to fill the rectangle as
     int 0x80
     add esp, 8
     pop ebp
@@ -155,18 +141,12 @@ cos32_video_rectangle_draw_block:
     push ebp
     mov ebp, esp
     mov eax, 11 ; Command 11 draw rectangle block
-    mov ebx, [ebp+8] ; The video rectangle
-    push ebx
-    mov ebx, [ebp+12] ; The pointer to the data to draw the block for
-    push ebx
-    mov ebx, [ebp+16] ; The absolute x position to draw on the rectangle
-    push ebx
-    mov ebx, [ebp+20] ; The absolute y position to draw on the rectangle
-    push ebx
-    mov ebx, [ebp+24] ; The total rows to draw on the rectangle
-    push ebx
-    mov ebx, [ebp+28] ; The pixels per row to draw
-    push ebx
+    push dword [ebp+8] ; The video rectangle
+    push dword [ebp+12] ; The pointer to the data to draw the block for
+    push dword [ebp+16] ; The absolute x position to draw on the rectangle
+    push dword [ebp+20] ; The absolute y position to draw on the rectangle
+    push dword [ebp+24]  ; The total rows to draw on the rectangle
+    push dword [ebp+28] ; THe pixels per row to draw
     int 0x80
     add esp, 24
     pop ebp
@@ -177,19 +157,13 @@ cos32_video_rectangle_draw_blocks:
     push ebp
     mov ebp, esp
     mov eax, 12 ; Command 12 draw rectangle blocks 
-    mov ebx, [ebp+8] ; The video rectangle
-    push ebx
-    mov ebx, [ebp+12] ; The pointer to the data to draw the block for
-    push ebx
-    mov ebx, [ebp+16] ; The absolute x position to draw on the rectangle
-    push ebx
-    mov ebx, [ebp+20] ; The absolute y position to draw on the rectangle
-    push ebx
-    mov ebx, [ebp+24] ; The total rows to draw on the rectangle
-    push ebx
-    mov ebx, [ebp+28] ; The pixels per row to draw
-    push ebx
-    mov ebx, [ebp+32] ; The total amount of pixel blocks in the provided array
+    push dword [ebp+8] ; The video rectangle
+    push dword [ebp+12] ; The pointer to the draw to draw the block for
+    push dword [ebp+16] ; The absolute x position to draw on the rectangle
+    push dword [ebp+20] ; THe absolute y position to draw on the rectangle
+    push dword [ebp+24] ; The total rows to draw on the rectangle
+    push dword [ebp+28] ; the pixels per row to draw
+    push dword [ebp+32] ; THe total amount of pixel blocks in the provided array
     int 0x80
     add esp, 28
     pop ebp
@@ -210,12 +184,9 @@ cos32_video_font_draw:
     push ebp
     mov ebp, esp
     mov eax, 14 ; Command 14 Draw character pixel data into buffer
-    mov ebx, [ebp+8] ; The pointer to the font
-    push ebx
-    mov ebx, [ebp+12] ; The pointer to the buffer we wish to put pixel data into
-    push ebx
-    mov ebx, [ebp+16] ; The pointer to the character array that we wish to draw as pixels
-    push ebx
+    push dword [ebp+8] ; The pointer to the font
+    push dword [ebp+12] ; THe pointer to the buffer we wish to put the pixel data into
+    push dword [ebp+16] ; The pointer to the character array that we wish to draw as pixels
     int 0x80
     add esp, 12
     pop ebp
@@ -225,10 +196,8 @@ cos32_video_font_make_empty_string:
     push ebp
     mov ebp, esp
     mov eax, 15 ; Command 15 create an empty string for font pixel data
-    mov ebx, [ebp+8] ; A pointer to the font
-    push ebx
-    mov ebx, [ebp+12] ; The pointer to the "len" integer specifying how many characters we need this buffer to store
-    push ebx
+    push dword [ebp+8] ; A pointer to the font
+    push dword [ebp+12] ; THe pointer to the "len" integer specifying how many characters we need this buffer to store 
     int 0x80
     add esp, 8
     pop ebp
@@ -238,18 +207,12 @@ cos32_video_rectangle_draw_font_data:
     push ebp
     mov ebp, esp
     mov eax, 16 ; Command 16 draws the given font pixel data into the rectangle
-    mov ebx, [ebp+8] ; Rectangle
-    push ebx
-    mov ebx, [ebp+12] ; Font : video_font
-    push ebx
-    mov ebx, [ebp+16] ; The pixel buffer that contains the pixel blocks
-    push ebx
-    mov ebx, [ebp+20] ; The absolute x coordinate to draw in this rectangle
-    push ebx
-    mov ebx, [ebp+24] ; The absolute y coordinate to draw in this rectangle
-    push ebx
-    mov ebx, [ebp+28] ; The total number of pixel characters stored in the buffer
-    push ebx
+    push dword [ebp+8] ; Rectangle
+    push dword [ebp+12] ; FOnt : video_font
+    push dword [ebp+16] ; The pixel buffer that contains the pixel blocks
+    push dword [ebp+20] ; THe absolute x coordiante to tdraw in this rectangle
+    push dword [ebp+24] ; The absolute y coordinate to draw in this rectangle
+    push dword [ebp+28] ; The total number of pixel characters stored in the buffer
     int 0x80
     add esp, 24
     pop ebp
@@ -260,10 +223,8 @@ cos32_video_rectangle_publish:
     push ebp
     mov ebp, esp
     mov eax, 17 ; Command 17 publishes the given rectangle
-    mov ebx, [ebp+8] ; Rectangle
-    push ebx
-    mov ebx, [ebp+12] ; The address of the name of the rectangle
-    push ebx
+    push dword [ebp+8] ; The rectangle
+    push dword [ebp+12] ; The address of the name of the rectangle
     int 0x80
     add esp, 8
     pop ebp
@@ -274,8 +235,7 @@ cos32_video_rectangle_get:
     push ebp
     mov ebp, esp
     mov eax, 18 ; Command 18 get the published rectangle by name
-    mov ebx, [ebp+8] ; Rectangle name
-    push ebx
+    push dword [ebp+8] ; The rectangle name
     int 0x80
     add esp, 4
     pop ebp
@@ -286,8 +246,7 @@ cos32_get_arguments:
     push ebp
     mov ebp, esp
     mov eax, 19 ; Command 19 get the process arguments, argc, and argv.
-    mov ebx, [ebp+8] ; The process_arguments structure to be populated
-    push ebx
+    push dword [ebp+8] ; The process_arguments structure to be populated
     int 0x80    ; Invoke the kernel!
     add esp, 4
     pop ebp
