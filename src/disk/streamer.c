43607dde14013feaadd2c8f2fb87066ec6149d68
Daniel McCarthy/AKA NibbleBits <dansblackberry2@hotmail.co.uk>
2020-12-03 18:49:16 +0000

    Fixed a big bug with the streamer, it didnt take into account that offsets combined with read length may overflow the buffer

--- src/disk/streamer.c
+++ src/disk/streamer.c
@@ -1,6 +1,7 @@
 #include "streamer.h"
 #include "memory/kheap.h"
 #include "config.h"
+#include <stdbool.h>
 
 struct disk_stream* diskstreamer_new(int disk_id)
 {
@@ -26,7 +27,17 @@ int diskstreamer_read(struct disk_stream* stream, void* out, int total)
 {
     int sector = stream->pos / COS32_SECTOR_SIZE;
     int offset = stream->pos % COS32_SECTOR_SIZE;
+    int total_to_read = total;
+    bool overflow = (offset+total_to_read) >= COS32_SECTOR_SIZE;
+
     char buf[COS32_SECTOR_SIZE];
+
+    // If we are going to overflow the buffer then we will need to change the amount we are planning
+    // to read and process the rest later on.
+    if (overflow)
+    {
+        total_to_read -= (offset+total_to_read) - COS32_SECTOR_SIZE;
+    }
 
     int res = disk_read_block(stream->disk, sector, 1, buf);
     if (res < 0)
@@ -34,7 +45,7 @@ int diskstreamer_read(struct disk_stream* stream, void* out, int total)
         goto out;
     }
 
-    int total_to_read = total > COS32_SECTOR_SIZE ? COS32_SECTOR_SIZE : total;
+
     for (int i = 0; i < total_to_read; i++)
     {
         *(char*)out++ = buf[offset+i];
@@ -42,11 +53,12 @@ int diskstreamer_read(struct disk_stream* stream, void* out, int total)
 
     // Adjust stream
     stream->pos += total_to_read;
-    // Do we have more to read? If the total is above the sector size we do
-    if (total > COS32_SECTOR_SIZE)
+    
+    // Do we have more to read? If we overflowed the sector we do
+    if (overflow)
     {
         // However we just read a sector! So we should minus it from the total to read
-        res = diskstreamer_read(stream, out, total-COS32_SECTOR_SIZE);
+        res = diskstreamer_read(stream, out, total-total_to_read);
     }
 out:
     return res;
